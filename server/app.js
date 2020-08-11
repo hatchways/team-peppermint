8d86ebf20aab38564c4e8abb3f64a1a41f03c147
Andrey Li <lee1975uz@gmail.com>
2020-08-11 16:40:19 -0600

    add invitations router in app.js on server side

--- server/app.js
+++ server/app.js
@@ -7,6 +7,9 @@ const logger = require("morgan");
 const indexRouter = require("./routes/index");
 const pingRouter = require("./routes/ping");
 
+
+const invitationsRouter = require("./routes/invitations");
+
 const { json, urlencoded } = express;
 
 var app = express();
@@ -19,6 +22,9 @@ app.use(express.static(join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/ping", pingRouter);
+
+
+app.use("/user/:id/invitations", invitationsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server/routes/invitations.js
+++ server/routes/invitations.js
@@ -7,7 +7,7 @@ router.get("/user/:id/invitations", async (req, res) => {
     //get all invitatitions by user id
     const invitations = await Invitation.find({ To_user: req.body.userEmail });
     //send response with invitations
-    res.json({ invitaions: invitations });
+    res.json(invitaions);
   } catch (err) {
     res.status(400).json({ err });
   }
