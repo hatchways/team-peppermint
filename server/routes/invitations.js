c4df11340f4d7cc75aeb3284eaa1c041599f876d
Andrey Li <lee1975uz@gmail.com>
2020-08-11 15:44:05 -0600

    add get invitations route

---
+++ server/routes/invitations.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const Invitaion = require("../models/Invitaion");
+
+//get invitation route
+router.get("/user/:id/invitations", async (req, res) => {
+  try {
+    //get all invitatitions by user id
+    const invitations = await Invitation.find({ To_user: req.body.userEmail });
+    //send response with invitations
+    res.json({ invitaions: invitations });
+  } catch (err) {
+    res.status(400).json({ err });
+  }
+});
+
+module.exports = router;
