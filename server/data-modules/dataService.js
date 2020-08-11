72b8f13b8fe5334bf0b74f909e714ccb1a218aa2
Jesse Suderman <62080718+C0dras1@users.noreply.github.com>
2020-08-10 18:47:00 -0700

    Updated dataService.js for userSchema

--- server/data-modules/dataService.js
+++ server/data-modules/dataService.js
@@ -1,13 +1,29 @@
 const { use } = require('chai');
+const mongoose = require('mongoose');
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = process.env.uri;
 const client = (callBack)=> MongoClient.connect(uri,{ useUnifiedTopology: true }, callBack);
-const userSchema = {
-    "_id": 0,
-    "email": "",
-    "password": ""
-}
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    max: 255,
+    min: 6
+  },
+  email: {
+    type: String,
+    required: true,
+    max: 255,
+    min: 6
+  },
+  password: {
+    type: String,
+    required: true,
+    max: 255,
+    min: 6
+  }
+})
 module.exports = function(){
     return{
         testConnection: function(){ 
