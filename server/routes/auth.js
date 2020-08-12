16fb90a77906e680c6c45dcf57e34c6cf67d4acb
olzhas828 <olzhas828@gmail.com>
2020-08-11 20:51:00 -0400

    changed signup/login routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,60 +2,53 @@ const router = require('express').Router();
 const UserSchema = require('../models/userSchema');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-
 const dataService = require('../data-modules/dataService');
 const data = dataService();
-
 
 //middleware validation
 const {signupValidation, loginValidation} = require('../middleware/validation');
 
-
 //signup route
 router.post('/signup', async (req, res) => {
-  let userExists;
+  
   //Valiate the data before adding new user
   const { error } = signupValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   //Check if the user is already in the database
-  data.getUserByEmail(req.body.email)
-  .then((user)=>{
-    if(user) userExists =true; 
-  })
+  const user = await data.getUserByEmail(req.body.email);
+  if(user) return res.status(400).send("User already exists");
+  
+  //Hash the pass
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(req.body.password, salt);
 
-  if(userExists){
-    res.status(400).send("User already exists");
-  }
-  else{
-    //Hash the pass
-    const salt = await bcrypt.genSalt(10);
-    const hashPassword = await bcrypt.hash(req.body.password, salt);
-    // create a new user
-    const user = {
-      name: req.body.name,
-      email: req.body.email,
-      password: hashPassword,
-    };  
-    data.createUser(user)
-    .then((msg)=>{
-      res.status(201).send(msg);
-    })
-    .catch((err)=>{
-      console.error(err);
-      res.status(400).send("error"+err);
-    })
-  }
+  // create a new user
+  const newUser = {
+    name: req.body.name,
+    email: req.body.email,
+    password: hashPassword,
+  };  
+  data.createUser(newUser)
+  .then((msg)=>{
+    res.status(201).send(msg);
+  })
+  .catch((err)=>{
+    console.error(err);
+    res.status(400).send("error"+err);
+  })
+  
 })
 
 //login route
 router.post('/login', async (req, res) => {
+  
   //validate the data before logging in
   const { error } = loginValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   //check if the user is already in the database
-  const user = await UserSchema.findOne({email: req.body.email});
+  const user = await data.getUserByEmail(req.body.email);
   if (!user) return res.status(400).send('Email not found');
 
   //checking password is correct
@@ -66,9 +59,10 @@ router.post('/login', async (req, res) => {
   try {
     const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
     res.header('auth-token', token).send(token);
+    console.log("logged");
   } catch (err) {
     console.log(err)
   }
 });
 
-module.exports = router
+module.exports = router;
