345190e043688c395fd4b835cd385ac522f6b38f
olzhas828 <olzhas828@gmail.com>
2020-08-11 21:36:27 -0400

    send mail function

--- server/data-modules/SendInvitation.js
+++ server/data-modules/SendInvitation.js
@@ -1,10 +1,10 @@
 const sgMail = require('@sendgrid/mail');
-
+sgMail.setApiKey(process.env.SG_API_KEY);
 
 var sendInvite = function(reciever){
     let msg ={
         to: reciever,
-        from: "hws.peppermint@gmail.com",
+        from: "pepperminthw@gmail.com",
         subject: 'Join to the app',
         text: 'Chat with people from different contries'
     }
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,24 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const invitation = require('../data-modules/SendInvitation');
-
-
-const sgMail = require('@sendgrid/mail');
 
 router.get("/welcome", function (req, res, next) { 
-  //invitation.sendInvite("olzhas828@gmail.com").then(()=>{
-    sgMail.setApiKey(process.env.sg_api);
-
-
-    const msg = {
-      to: 'pepperminthw@gmail.com',
-      from: 'pepperminthw@gmail.com',
-      subject: 'Sending with Twilio SendGrid is Fun',
-      text: 'and easy to do anywhere, even with Node.js',
-      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-    };
-    sgMail.send(msg).catch((err)=>console.error(err));;
     res.status(200).send({ welcomeMessage: "Step 1 (completed)" }); 
-  //}).catch((err)=>{console.error(err)});
 });
 module.exports = router;
