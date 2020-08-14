b39f7400e21248286a54882cb411320a89c6fceb
Andrey Li <lee1975uz@gmail.com>
2020-08-14 15:31:56 -0600

    add user email to user image name to make it unique

--- client/src/components/ContactItem/index.jsx
+++ client/src/components/ContactItem/index.jsx
@@ -1,19 +1,26 @@
-import React, { memo, useState } from "react";
+import React, { memo, useState, useCallback } from "react";
 import { useStyles } from "./style";
 import PropTypes from "prop-types";
 import UserAvatar from "../UserAvatar";
 import { Typography, ListItem, ButtonBase } from "@material-ui/core";
-import { MoreHoriz } from "@material-ui/icons";
 import { DropzoneDialog } from "material-ui-dropzone";
+import { MoreHoriz } from "@material-ui/icons";
 import uploadUserImage from "../../services/uploadUserImage";
 
 const ContactItem = (props) => {
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);  
   const classes = useStyles();
   const { name, index } = props;
 
   const handleSave = (files) => {
-    uploadUserImage(files[0]);
+    // add user email to make user avatar unique
+    Object.defineProperties(files[0], {
+      name: {
+        value: "ya@ya.ru" + files[0].name,
+        writable: true,
+      },
+    });    
+    uploadUserImage(files[0])
     setOpen(false);
   };
 
@@ -40,6 +47,7 @@ const ContactItem = (props) => {
         onSave={handleSave}
         showPreviews={true}
         maxFileSize={5000000}
+        onClose={() => setOpen(false)}
       />
     </ListItem>
   );
