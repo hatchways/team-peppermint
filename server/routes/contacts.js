a4758089ae5a0e4d25a40828913c3dbefc02f47a
Andrey Li <lee1975uz@gmail.com>
2020-08-18 15:42:51 -0600

    in contacts.js replaced returned value with contacts

--- server/routes/contacts.js
+++ server/routes/contacts.js
@@ -3,10 +3,10 @@ const data = require("../data-modules/dataService")();
 router.get("/:email/contacts", async (req, res) => {
   try {
     //find contacts by user id
-    const user = await data.getContacts(req.params.email);
+    const contacts = await data.getContacts(req.params.email);
 
     //response with contacts
-    res.status(200).send(user);
+    res.status(200).json(contacts);
   } catch (err) {
     res.status(400).json(err);
   }
