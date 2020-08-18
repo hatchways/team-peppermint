9f810bd349092881214469d311bbb84ace11dfbd
olzhas828 <olzhas828@gmail.com>
2020-08-18 12:17:07 -0400

    models updated

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -1,5 +1,21 @@
 const mongoose = require('mongoose');
 
+const contactSchema = new mongoose.Schema({
+    email:{
+        type: String,
+        required: true,
+        max: 255,
+        min: 6
+    },
+    status:{ //0-pending, 1-accepted, 2-rejected
+        type: Number,
+        default: 0,
+    },
+    conversationID:{
+        type: String
+    }
+},{ _id: false })
+
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -20,7 +36,8 @@ const UserSchema = new mongoose.Schema({
     min: 6
   },
   contacts: {
-    type: Array
+    type: [contactSchema],
+    default: []
   }
 })
 
