4eaf47f3cdd282f7704df5b4f23a1a6f4850c6b0
Andrey Li <lee1975uz@gmail.com>
2020-10-25 12:36:37 -0600

    add CI=false to netlify.toml

---
+++ client/build/precache-manifest.e8d028164fa0cf748c1c19a0a2c0654d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42a2e7d3ec329f1de2c4ef2053c06fcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fb0a18919ae8c208943",
+    "url": "/static/css/4.c2bc211b.chunk.css"
+  },
+  {
+    "revision": "689c277442bfb081aa21",
+    "url": "/static/css/main.692d75bb.chunk.css"
+  },
+  {
+    "revision": "da713cf32ef74454da4e",
+    "url": "/static/js/0.8ad6ddd7.chunk.js"
+  },
+  {
+    "revision": "bbd70c5ad53561396542",
+    "url": "/static/js/1.1e5944df.chunk.js"
+  },
+  {
+    "revision": "d985de5d55e0e6dbadee53aa909f70c1",
+    "url": "/static/js/1.1e5944df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fb0a18919ae8c208943",
+    "url": "/static/js/4.b9f3e27e.chunk.js"
+  },
+  {
+    "revision": "bddfdd43fc66fd206a080c4c7f0d57eb",
+    "url": "/static/js/4.b9f3e27e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf953f2053e40d552d09",
+    "url": "/static/js/5.48a11ed4.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/static/js/5.48a11ed4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f38c1a6906ad80933d3",
+    "url": "/static/js/6.b1aab95c.chunk.js"
+  },
+  {
+    "revision": "0f556c9a5238ff0fff12",
+    "url": "/static/js/7.4ddf7f21.chunk.js"
+  },
+  {
+    "revision": "8666a5f1b227ea60bde1",
+    "url": "/static/js/8.5ea88ba4.chunk.js"
+  },
+  {
+    "revision": "689c277442bfb081aa21",
+    "url": "/static/js/main.67d28e5d.chunk.js"
+  },
+  {
+    "revision": "550367ecd8370a902622",
+    "url": "/static/js/runtime-main.2312d961.js"
+  },
+  {
+    "revision": "f1bf8dfd265ba3d536667f5ad826c9c5",
+    "url": "/static/media/avatar.f1bf8dfd.png"
+  },
+  {
+    "revision": "48ff3fdae9816b781ea671973711d740",
+    "url": "/static/media/login-signup-bgImage.48ff3fda.png"
+  }
+]);
